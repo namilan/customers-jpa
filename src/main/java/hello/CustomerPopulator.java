53a82a231c2c277a87fbf993dc6b7265e131fae1
namilan <nichole.milan@gmail.com>
2019-06-30 10:43:23 -0400

    first commit

---
+++ src/main/java/hello/CustomerPopulator.java
@@ -0,0 +1,71 @@
+package hello;
+
+import java.util.Optional;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerPopulator implements CommandLineRunner {
+
+	private Logger log = LoggerFactory.getLogger(CustomerPopulator.class);
+
+	@Resource
+	private CustomerRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// save a couple of customers
+		repository.save(new Customer("Jack", "Bauer"));
+		repository.save(new Customer("Chloe", "O'Brian"));
+		repository.save(new Customer("Kim", "Bauer"));
+		repository.save(new Customer("David", "Palmer"));
+		repository.save(new Customer("Michelle", "Dessler"));
+		
+		
+		// fetch all customers
+		log.info("Customers found with findAll():");
+		log.info("-------------------------------");
+		for (Customer customer : repository.findAll()) {
+			log.info(customer.toString());
+		}
+		log.info("");
+
+		// fetch an individual customer by ID
+		Optional<Customer> customer = repository.findById(1L);
+		log.info("Customer found with findOne(1L):");
+		log.info("--------------------------------");
+		log.info(customer.toString());
+		log.info("");
+
+		// fetch customers by last name
+		log.info("Customer found with findByLastName('Bauer'):");
+		log.info("--------------------------------------------");
+		for (Customer bauer : repository.findByLastName("Bauer")) {
+			log.info(bauer.toString());
+		}
+		log.info("");
+		
+		// fetch all customers
+				log.info("Customers found with findAll() and sorted by last name:");
+				log.info("-------------------------------");
+				for (Customer customers: repository.findAllByOrderByLastNameAsc()) {
+					log.info(customer.toString());
+				}
+				log.info("");
+				
+		// fetch all 3 customers
+				log.info("First 3 customers found with findAll() and sorted by last name:");
+				log.info("-------------------------------");
+				for (Customer customers: repository.findFirst3ByOrderByLastNameAsc()) {
+					log.info(customer.toString());
+				}
+				log.info("");
+
+		
+	}
+}
